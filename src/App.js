a9f4200bdc701f192514d71b7325a5d8c88b460e
Anna Moura Sobral <annamourasobral@gmail.com>
2022-07-17 19:43:23 +0200

    base to crete components

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <img src={logo} width="40px" className="App-logo" alt="logo" />
+        </nav>
       </header>
+      <main>
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
+      <footer>
+        <small> © 2022 @annamourasobral development. All rights reserved. </small>
+      </footer>
     </div>
   );
 }
