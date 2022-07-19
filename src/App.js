c67d12f5277f94621eef2cdeec7e9e9a6c8dfe8a
Anna Moura Sobral <annamourasobral@gmail.com>
2022-07-19 11:30:03 +0200

    organazing components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import Header from "./Header"
+import Main from "./Main"
+import Footer from "./Footer"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav>
-          <img src={logo} width="40px" className="App-logo" alt="logo" />
-        </nav>
-      </header>
-      <main>
-        <h1>Fun facts about React</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100K stars on GitHub</li>
-          <li>Is maintained by Facebook</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-      </main>
-      <footer>
-        <small> © 2022 @annamourasobral development. All rights reserved. </small>
-      </footer>
+    <div>
+        <Header />
+        <Main />
+        <Footer />
     </div>
   );
 }
